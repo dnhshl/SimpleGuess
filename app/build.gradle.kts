c0fe03865a24b8fc6582172598448b29b2e660b0
Detlev Noll <dn@experikus.de>
2025-03-03 11:02:55 +0100

    Simple Guess ohne ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.main.simpleguess"
+        applicationId = "com.example.main.simpleguess2025"
         minSdk = 29
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,14 +17,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -34,13 +31,13 @@ import com.example.main.ui.theme.SimpleCalc2025Theme
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SimpleCalc2025Theme {
-                MyScreen(viewModel = viewModel)
+                MyScreen()
             }
         }
     }
@@ -48,19 +45,25 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MyScreen(viewModel: MainViewModel) {
+fun MyScreen() {
 
-    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
 
+    var numberToGuess by remember { mutableStateOf(Random.nextInt(1, 101)) }
+    var entry by remember { mutableStateOf("") }
+    var isPlaying by remember { mutableStateOf(false) }
+    var result by remember { mutableStateOf("") }
+    var isCorrect by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
 
 
     // Prüfen ob die Eingabe eine Zahl ist
-    val isValidEntry = uiState.entry.toIntOrNull() != null
+    val isValidEntry = entry.toIntOrNull() != null
 
 
     // Variable Info Anzeige in Abhängigkeit von isPlaying
     var info = when {
-        !uiState.isPlaying -> stringResource(id = R.string.info_message)
+        !isPlaying -> stringResource(id = R.string.info_message)
         else -> stringResource(id = R.string.error_message)
     }
 
@@ -92,10 +95,21 @@ fun MyScreen(viewModel: MainViewModel) {
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 // Eingabefeld für die Zahl
-                IntegerInputField(value = uiState.entry, onInputChanged = { viewModel.onEntryChanged(it) })
+                IntegerInputField(value = entry, onInputChanged = { entry = it })
                 // Button zur Bestätigung der Eingabe; nur aktiv wenn eine Zahl eingegeben wurde
                 Button(
-                    onClick = { viewModel.onClickGuessNumberButton() },
+
+                    onClick = {
+                        val guess = entry.toInt()
+                        result = when {
+                            guess < numberToGuess -> context.getString(R.string.too_low)
+                            guess > numberToGuess -> context.getString(R.string.too_high)
+                            else -> context.getString(R.string.correct)
+                        }
+                        isPlaying = (guess != numberToGuess)
+                        isCorrect = (guess == numberToGuess)
+                        counter++
+                    },
                     enabled = isValidEntry
                 ) {
                     Text(text = stringResource(id = R.string.guess_number))
@@ -104,24 +118,29 @@ fun MyScreen(viewModel: MainViewModel) {
             Spacer(modifier = Modifier.height(30.dp))
             // Button zum Starten des Spiels
             Button(
-                onClick = { viewModel.onClickStartGameButton() },
-                ) {
+                onClick = {
+                    numberToGuess = Random.nextInt(1, 101)
+                    isPlaying = true
+                    result = ""
+                    counter = 0
+                    isCorrect = false
+                },
+            ) {
                 Text(text = stringResource(id = R.string.start_game))
             }
             Spacer(modifier = Modifier.height(30.dp))
             // Anzeige des aktuellen Ergebnisses
             Text(
-                text = uiState.result,
+                text = result,
                 style = MaterialTheme.typography.headlineLarge
             )
             // Bei korrekter Eingabe Anzeige der Versuche
-            if (uiState.isCorrect) {
+            if (isCorrect) {
                 Text(
-                    text = stringResource(id = R.string.attempts, uiState.counter),
+                    text = stringResource(id = R.string.attempts, counter),
                     style = MaterialTheme.typography.bodySmall
                 )
             }
         }
     }
 }
-
